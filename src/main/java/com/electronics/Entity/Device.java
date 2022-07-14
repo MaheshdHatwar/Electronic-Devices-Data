2e47a141022010db5cbb422b251ac056934f92a4
Mahesh Hatwar <mahesh.hatwar@mindtree.com>
2022-07-14 19:02:52 +0530

    forth commit

--- src/main/java/com/electronics/Entity/Device.java
+++ src/main/java/com/electronics/Entity/Device.java
@@ -19,45 +19,6 @@ public class Device {
 	@Id
 	@GeneratedValue
 	private long id;
-	public Device() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public Device(long id, String name, String category, long price) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.category = category;
-		this.price = price;
-	}
-	@Override
-	public String toString() {
-		return "Device [id=" + id + ", name=" + name + ", category=" + category + ", price=" + price + "]";
-	}
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getCategory() {
-		return category;
-	}
-	public void setCategory(String category) {
-		this.category = category;
-	}
-	public long getPrice() {
-		return price;
-	}
-	public void setPrice(long price) {
-		this.price = price;
-	}
 	private String name;
 	private String category;
 	private long price;
